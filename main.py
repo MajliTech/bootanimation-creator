12e35fd4fe49c8fd495d8d4f539d8211a8160263
MajliTech <dev@majlitech.pl>
2022-12-28 20:38:47 +0100

    Add LICENSE file

---
+++ main.py
@@ -0,0 +1,10 @@
+# ---------------------------------------------------------------------
+# BootAnimation Creator
+# By MajliTech
+# You should have a LICENSE file attached to this project
+# If not, please see https://spdx.org/licenses/LGPL-3.0-or-later.html
+# ---------------------------------------------------------------------
+
+
+#Libraries
+import colorama
